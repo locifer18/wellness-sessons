8455f3adeeb840a4fde3cf3b9d58fd233eef6f93
Ansh Kanda <167521162+locifer18@users.noreply.github.com>
2025-08-01 02:04:36 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URl);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(
             `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
         );
